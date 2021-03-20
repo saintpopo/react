6aa2f09161e6688b650a1b1d7a2c8869c2e4d9ab
saintpopo <gaksidjax1614@gmail.com>
2021-03-21 02:17:27 +0530

    project setup

---
+++ portfolio/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+      <h2>Gaurav Singh</h2>
+  );
+}
+
+export default App;
